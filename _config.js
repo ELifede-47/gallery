ac2b463fb0293cc5cad96eacc71260cd5edd841b
ELifede-47 <elvis.lifede@student.moringaschool.com>
2025-04-26 16:51:24 +0300

    Milestone 2 - Updated landing page and Jenkinsfile

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://elvislifede:HkOSAYkZmU6X6Nm6@ip01.sbtnrb6.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://elvislifede:HkOSAYkZmU6X6Nm6@ip01.sbtnrb6.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://elvislifede:HkOSAYkZmU6X6Nm6@ip01.sbtnrb6.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
-module.exports = config;
+module.exports = {
+    DB: 'mongodb+srv://elvislifede:HkOSAYkZmU6X6Nm6@cluster0.abcd.mongodb.net/myAppDB?retryWrites=true&w=majority'
+};
